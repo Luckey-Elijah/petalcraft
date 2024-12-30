e368c183b4a1899e93e15aa7f38b79f551a5162c
Elijah Luckey <luckeyelijah112@gmail.com>
2024-12-30 15:36:13 -0500

    Refactor player data structure to encapsulate properties and improve code organization

--- player.lua
+++ player.lua
@@ -1,11 +1,10 @@
-local player = {}
-local quads = {}
 local currentFrame = 1
 local frameTime = 0.1 -- Time per frame
 local timer = 0
 local playerSprite
 
-local data = {
+local player = {
+    quads = {},
     position = { x = 100, y = 100 },
     speed = 200,
     width = 32,
@@ -17,9 +16,10 @@ local data = {
 
 function player.Load()
     playerSprite = love.graphics.newImage("assets/images/radish-man.png")
+
     local frameWidth, frameHeight = 32, 32
     for i = 0, (playerSprite:getWidth() / frameWidth) - 1 do
-        quads[#quads + 1] = love.graphics.newQuad(
+        player.quads[#player.quads + 1] = love.graphics.newQuad(
             i * frameWidth, 0, frameWidth, frameHeight, playerSprite:getDimensions()
         )
     end
@@ -27,34 +27,34 @@ end
 
 local function setBoost()
     if love.keyboard.isDown("space") then
-        data.boost = 2
+        player.boost = 2
     end
 end
 
 local function setVelocity(dt)
     if love.keyboard.isDown("w") then
-        data.velocity.y = -data.speed * data.boost
+        player.velocity.y = -player.speed * player.boost
     elseif love.keyboard.isDown("s") then
-        data.velocity.y = data.speed * data.boost
+        player.velocity.y = player.speed * player.boost
     else
-        data.velocity.y = data.velocity.y * (1 - data.friction * dt)
+        player.velocity.y = player.velocity.y * (1 - player.friction * dt)
     end
 
     if love.keyboard.isDown("a") then
-        data.velocity.x = -data.speed * data.boost
+        player.velocity.x = -player.speed * player.boost
     elseif love.keyboard.isDown("d") then
-        data.velocity.x = data.speed * data.boost
+        player.velocity.x = player.speed * player.boost
     else
-        data.velocity.x = data.velocity.x * (1 - data.friction * dt)
+        player.velocity.x = player.velocity.x * (1 - player.friction * dt)
     end
 end
 
 
 local function setPostion(dt)
-    data.velocity.x = data.velocity.x * (1 - data.friction * dt)
-    data.velocity.y = data.velocity.y * (1 - data.friction * dt)
-    data.position.x = data.position.x + data.velocity.x * dt
-    data.position.y = data.position.y + data.velocity.y * dt
+    player.velocity.x = player.velocity.x * (1 - player.friction * dt)
+    player.velocity.y = player.velocity.y * (1 - player.friction * dt)
+    player.position.x = player.position.x + player.velocity.x * dt
+    player.position.y = player.position.y + player.velocity.y * dt
 end
 
 
@@ -62,7 +62,7 @@ local function setAnimation(dt)
     timer = timer + dt
     if timer > frameTime then
         timer = 0
-        currentFrame = currentFrame % #quads + 1
+        currentFrame = currentFrame % #player.quads + 1
     end
 end
 
@@ -74,7 +74,7 @@ function player.Update(dt)
 end
 
 function player.Draw()
-    love.graphics.draw(playerSprite, quads[currentFrame], data.position.x, data.position.y)
+    love.graphics.draw(playerSprite, player.quads[currentFrame], player.position.x, player.position.y)
 end
 
 return player
