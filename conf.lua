d1876f35cddcff50026c667904cb0955f7a3d339
Elijah Luckey <luckeyelijah112@gmail.com>
2024-12-30 19:37:18 -0500

    I'm so embarrased i don't even know how to spell my ow games name

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@ function love.conf(t)
     t.window.width = 400
     t.window.height = 400
     t.window.resizable = true
-    t.window.title = "Pedalcraft"
+    t.window.title = "Petalcraft"
     t.window.icon = "assets/images/radish-man.png"
     t.author = "APEALED"
 end
